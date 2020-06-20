5b98b45d6bb0d39809df969a3ff0f5cd2f8939a2
izabong <e0424929@u.nus.edu>
2020-06-20 10:01:43 +0800

    18/6/2020

--- app/src/main/java/com/example/giveapp/rfo_stepcounter.java
+++ app/src/main/java/com/example/giveapp/rfo_stepcounter.java
@@ -2,13 +2,81 @@ package com.example.giveapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class rfo_stepcounter extends AppCompatActivity {
+
+    private TextView stepCounter;
+    private double magnitudePrev = 0;
+    private Integer count = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rfo_stepcounter);
+
+        stepCounter = findViewById(R.id.numSteps);
+        SensorManager sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        Sensor sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        SensorEventListener stepDetector = new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent event) {
+                if (event!=null) {
+                    float x_acceleration = event.values[0];
+                    float y_acceleration = event.values[1];
+                    float z_acceleration = event.values[2];
+
+                    double magnitude = Math.sqrt(x_acceleration*x_acceleration + y_acceleration*y_acceleration + z_acceleration*z_acceleration);
+                    double magnitudeDelta = magnitude - magnitudePrev;
+                    magnitudePrev = magnitude;
+
+                    if (magnitudeDelta > 6) { // 10 for running
+                        count++;
+                    }
+                    stepCounter.setText(count.toString());
+                }
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+            }
+        };
+
+        sensorManager.registerListener(stepDetector, sensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    protected void onPause() {
+        super.onPause();
+
+        SharedPreferences sharedPreferences = getPreferences(MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.clear();
+        editor.putInt("count", count);
+        editor.apply();
+    }
+
+    protected void onStop() {
+        super.onStop();
+
+        SharedPreferences sharedPreferences = getPreferences(MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.clear();
+        editor.putInt("count", count);
+        editor.apply();
+    }
+
+    protected void onResume() {
+        super.onResume();
+
+        SharedPreferences sharedPreferences = getPreferences(MODE_PRIVATE);
+        count = sharedPreferences.getInt("count", 0);
     }
 }
